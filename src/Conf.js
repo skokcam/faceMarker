0f69f1136ed435f35e4f2b9864f85d97b5d07d4f
Said Kokcam <saidkokcam@gmail.com>
2022-04-27 20:55:40 +0300

    updated server config

--- src/Conf.js
+++ src/Conf.js
@@ -1,7 +1,10 @@
 // Configuration for frontend
 
 //Check if are running local or deployed somewhere
-const server = window.location.origin !== 'http://localhost:3001' ? window.location.origin:'http://localhost:3000'
+let server = (window.location.origin !== 'http://localhost:3001') ? window.location.origin:'http://localhost:3000';
+if (server.includes('github.io')) {
+  server = 'https://rocky-spire-68123.herokuapp.com'
+}
 
 const Conf = {
     server: server
